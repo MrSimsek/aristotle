dc7bdbc285d6e80a7e22cf0666f02d9741e42fea
Deniz Simsek <denizsimsek93@gmail.com>
2021-07-16 11:55:44 +0300

    Add Auth Layout

---
+++ src/layouts/AuthLayout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './authLayout.styles.css';
+
+type AuthLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function AuthLayout(props: AuthLayoutProps) {
+  const { children } = props;
+
+  return <div className={styles.authLayoutContainer}>{children}</div>;
+}
