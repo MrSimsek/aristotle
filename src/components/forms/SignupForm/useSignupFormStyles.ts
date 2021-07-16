d3e2e04243ca8fd7c4bf202a7005563bfcf9ccde
Deniz Simsek <denizsimsek93@gmail.com>
2021-07-16 14:56:54 +0300

    Change to JSS

---
+++ src/components/forms/SignupForm/useSignupFormStyles.ts
@@ -0,0 +1,10 @@
+import { createUseStyles } from 'react-jss';
+
+const useSignupFormStyles = createUseStyles({
+  formContainer: {
+    margin: 'auto',
+    width: '90%',
+  },
+});
+
+export default useSignupFormStyles;
