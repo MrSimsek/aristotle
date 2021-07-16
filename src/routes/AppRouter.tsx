cfedc5c0d919d53ccc69a4fbdd54dacdbbaf2f19
Deniz Simsek <denizsimsek93@gmail.com>
2021-07-16 14:04:26 +0300

    Add route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import SignupPage from '../containers/pages/SignupPage';
+import SigninPage from '../containers/pages/SigninPage';
 
 export default function AppRouter() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={SignupPage} />
+        <Route exact path="/" component={SigninPage} />
+        <Route path="/signup" component={SignupPage} />
       </Switch>
     </Router>
   );
