69d72f69df3a13a3512f13256d2970fb6e9fb7b8
Deniz Simsek <denizsimsek93@gmail.com>
2021-07-17 15:13:05 +0300

    Add placeholder user page

---
+++ src/containers/pages/DashboardPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import AuthLayout from '../../layouts/AuthLayout';
+
+export default function DashboardPage() {
+  return (
+    <AuthLayout>
+      <p style={{ textAlign: 'center' }}>Welcome! This is the user page.</p>
+    </AuthLayout>
+  );
+}
