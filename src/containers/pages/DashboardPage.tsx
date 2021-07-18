42ac8c29bfdb3fe40f3e1eb648ddb9f8d11686b7
Deniz Simsek <denizsimsek93@gmail.com>
2021-07-18 10:25:55 +0300

    Add menu items

--- src/containers/pages/DashboardPage.tsx
+++ src/containers/pages/DashboardPage.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import {
+  BarChartOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
 
 import DashboardLayout from '../../layouts/DashboardLayout';
 
+import DashboardHomePage from '../dashboard/DashboardHomePage';
+
 export default function DashboardPage() {
-  return (
-    <DashboardLayout>
-      <p style={{ textAlign: 'center' }}>Welcome! This is the user page.</p>
-    </DashboardLayout>
-  );
+  const menuItems = [
+    {
+      key: 'page-1',
+      title: 'Page 1',
+      content: <DashboardHomePage />,
+      icon: <UserOutlined />,
+    },
+    {
+      key: 'page-2',
+      title: 'Page 2',
+      content: <div>Page 2</div>,
+      icon: <VideoCameraOutlined />,
+    },
+    {
+      key: 'page-3',
+      title: 'Page 3',
+      content: <div>Page 3</div>,
+      icon: <UploadOutlined />,
+    },
+    {
+      key: 'page-4',
+      title: 'Page 4',
+      content: <div>Page 4</div>,
+      icon: <BarChartOutlined />,
+    },
+  ];
+
+  return <DashboardLayout menuItems={menuItems} />;
 }
